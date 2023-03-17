527ba2c30903ab0a6e094caf700579d09b89d2c8
Victor Cadillo <45715531+vcadillog@users.noreply.github.com>
2023-03-16 21:27:59 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,171 +1,214 @@
-import torch as th
+import os
+import time
+import random
+import argparse
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import cv2
+import PIL.Image
+
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+import torch
+from torch.utils.data import DataLoader, Dataset
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data.sampler import RandomSampler, SequentialSampler
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from util import GradualWarmupSchedulerV2
 
-from torch.utils.tensorboard import SummaryWriter
-from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms import ToTensor
-import glob
-from PIL import Image
+from dataset import get_df, get_transforms, MelanomaDataset
+from models import Effnet_Melanoma, Resnest_Melanoma, Seresnext_Melanoma
 
 
-class EnsClassificationDataset(Dataset):
-    def __init__(self, X,y,class_type):
-        self.dir ='/kaggle/input/jpeg-melanoma-256x256/'+class_type        
-        self.X = X    
-        self.y = th.from_numpy(y.to_numpy())   
-        self.class_type = class_type     
+def set_seed(seed=0):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
 
-    def __len__(self):
-        return len(self.X)    
 
-    def __getitem__(self, idx):
+def train_epoch(model, loader, optimizer):
 
-              
-        img = glob.glob(self.dir+'/'+self.X[idx][0]+'.jpg')        
-        X_tab = th.from_numpy(self.X[idx][1:].astype(float))
+    model.train()
+    train_loss = []
+    bar = tqdm(loader)
+    for (data, target) in bar:
 
-        X_img = Image.open(img[0])
-        X_img = ToTensor()(X_img)                     
-        if self.class_type == 'train':
-            y = self.y[idx]    
-            return X_img,X_tab.to(th.float32),y.type(th.LongTensor)
+        optimizer.zero_grad()
+        
+        if args.use_meta:
+            data, meta = data
+            data, meta, target = data.to(device), meta.to(device), target.to(device)
+            logits = model(data, meta)
         else:
-            return X_img,X_tab.to(th.float32)
-
-def dataloader(data_train,data_val,data_test,batch_size,shuffle = True, drop_last = False):
-    train_dataloader = DataLoader(data_train, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
-    test_dataloader = DataLoader(data_test, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
-    val_dataloader = DataLoader(data_val, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)   
-
-    train_img , train_tabular, train_y = next(iter(train_dataloader))
-    print(f"Image batch shape: {train_img.size()}")
-    print(f"Tabular batch shape: {train_tabular.size()}")
-    print(f"Classes batch shape: {train_y.size()}")
-    return train_dataloader,val_dataloader,test_dataloader,train_tabular.size()
-
-class EnsembleNetwork(nn.Module):
-    def __init__(self,input_dim: int) -> None:
-        super(EnsembleNetwork, self).__init__()    
-        self.fc1 = nn.Linear(input_dim, 18)        
-        self.fc2 = nn.Linear(18, 36)
-        self.fc3 = nn.Linear(36, 18)
-        self.fc4 = nn.Linear(18,input_dim)
-        self.output = nn.Linear(input_dim+1000, 1) # Size Output of resnet: 1000
-        self.relu = nn.ReLU()           
-         
-        self.resnet = th.hub.load('pytorch/vision:v0.10.0', 'resnet18')
-
-    def forward(self, img: th.Tensor, tab: th.Tensor) -> th.Tensor:              
+            data, target = data.to(device), target.to(device)
+            logits = model(data)        
         
-        tab = self.fc1(tab)
-        tab = self.relu(tab)
-        tab = self.fc2(tab)
-        tab = self.relu(tab)
-        tab = self.fc3(tab)
-        tab = self.relu(tab)
-        tab = self.fc4(tab)
-        tab = self.relu(tab)
-        
-        img = self.resnet(img) 
-        
-        x = th.cat((img,tab), 1)        
-        x = self.output(x)
-        x = self.relu(x)
-        
-        return x    
+        loss = criterion(logits, target)
+
+        if not args.use_amp:
+            loss.backward()
+#         else:
+#             with amp.scale_loss(loss, optimizer) as scaled_loss:
+#                 scaled_loss.backward()
+
+        if args.image_size in [896,576]:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        loss_np = loss.detach().cpu().numpy()
+        train_loss.append(loss_np)
+        smooth_loss = sum(train_loss[-100:]) / min(len(train_loss), 100)
+        bar.set_description('loss: %.5f, smth: %.5f' % (loss_np, smooth_loss))
+
+    train_loss = np.mean(train_loss)
+    return train_loss
+
+
+def get_trans(img, I):
+
+    if I >= 4:
+        img = img.transpose(2, 3)
+    if I % 4 == 0:
+        return img
+    elif I % 4 == 1:
+        return img.flip(2)
+    elif I % 4 == 2:
+        return img.flip(3)
+    elif I % 4 == 3:
+        return img.flip(2).flip(3)
+
+
+def val_epoch(model, loader, mel_idx, is_ext=None, n_test=1, get_output=False):
+
+    model.eval()
+    val_loss = []
+    LOGITS = []
+    PROBS = []
+    TARGETS = []
+    with torch.no_grad():
+        for (data, target) in tqdm(loader):
+            
+            if args.use_meta:
+                data, meta = data
+                data, meta, target = data.to(device), meta.to(device), target.to(device)
+                logits = torch.zeros((data.shape[0], args.out_dim)).to(device)
+                probs = torch.zeros((data.shape[0], args.out_dim)).to(device)
+                for I in range(n_test):
+                    l = model(get_trans(data, I), meta)
+                    logits += l
+                    probs += l.softmax(1)
+            else:
+                data, target = data.to(device), target.to(device)
+                logits = torch.zeros((data.shape[0], args.out_dim)).to(device)
+                probs = torch.zeros((data.shape[0], args.out_dim)).to(device)
+                for I in range(n_test):
+                    l = model(get_trans(data, I))
+                    logits += l
+                    probs += l.softmax(1)
+            logits /= n_test
+            probs /= n_test
+
+            LOGITS.append(logits.detach().cpu())
+            PROBS.append(probs.detach().cpu())
+            TARGETS.append(target.detach().cpu())
+
+            loss = criterion(logits, target)
+            val_loss.append(loss.detach().cpu().numpy())
+
+    val_loss = np.mean(val_loss)
+    LOGITS = torch.cat(LOGITS).numpy()
+    PROBS = torch.cat(PROBS).numpy()
+    TARGETS = torch.cat(TARGETS).numpy()
+
+    if get_output:
+        return LOGITS, PROBS
+    else:
+        acc = (PROBS.argmax(1) == TARGETS).mean() * 100.
+        auc = roc_auc_score((TARGETS == mel_idx).astype(float), PROBS[:, mel_idx])
+        auc_20 = roc_auc_score((TARGETS[is_ext == 0] == mel_idx).astype(float), PROBS[is_ext == 0, mel_idx])
+        return val_loss, acc, auc, auc_20
+
+
+def run(fold, df, meta_features, n_meta_features, transforms_train, transforms_val, mel_idx):
+
+    if args.DEBUG:
+        args.n_epochs = 5
+        df_train = df[df['fold'] != fold].sample(args.batch_size * 5)
+        df_valid = df[df['fold'] == fold].sample(args.batch_size * 5)
+    else:
+        df_train = df[df['fold'] != fold]
+        df_valid = df[df['fold'] == fold]
+
+    dataset_train = MelanomaDataset(df_train, 'train', meta_features, transform=transforms_train)
+    dataset_valid = MelanomaDataset(df_valid, 'valid', meta_features, transform=transforms_val)
+    train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=args.batch_size, sampler=RandomSampler(dataset_train), num_workers=args.num_workers)
+    valid_loader = torch.utils.data.DataLoader(dataset_valid, batch_size=args.batch_size, num_workers=args.num_workers)
+
+    model = ModelClass(
+        args.enet_type,
+        n_meta_features=n_meta_features,
+        n_meta_dim=[int(nd) for nd in args.n_meta_dim.split(',')],
+        out_dim=args.out_dim,
+        pretrained=True
+    )
+          
+    model = model.to(device)
+    if args.wandb:
+        wandb.watch(model)
+
+    auc_max = 0.
+    auc_20_max = 0.
+    model_file  = os.path.join(args.model_dir, f'{args.kernel_type}_best_fold{fold}.pth')
+    model_file2 = os.path.join(args.model_dir, f'{args.kernel_type}_best_20_fold{fold}.pth')
+    model_file3 = os.path.join(args.model_dir, f'{args.kernel_type}_final_fold{fold}.pth')
+
+    optimizer = optim.Adam(model.parameters(), lr=args.init_lr)
+
+    if DP:
+        model = nn.DataParallel(model)
+#     scheduler_cosine = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.n_epochs - 1)
+    scheduler_cosine = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, args.n_epochs - 1)
+    scheduler_warmup = GradualWarmupSchedulerV2(optimizer, multiplier=10, total_epoch=1, after_scheduler=scheduler_cosine)
     
+    print(len(dataset_train), len(dataset_valid))
 
-class learning_loop():
-    def __init__(self, N_EPOCH,model,device,train_data,val_data,test_data,optimizer,criterion, directory,mode ) -> None:
-        self.N_EPOCH = N_EPOCH
-        self.model = model
-        self.device = device
-        self.train_data = train_data
-        self.val_data = val_data
-        self.test_data = test_data
-        self.optimizer = optimizer
-        self.criterion = criterion        
-        self.mode = mode     
-        self.writer1=SummaryWriter('logs/'+directory+'/train',flush_secs=60)
-        self.writer2=SummaryWriter('logs/'+directory+'/val',flush_secs=60)   
+    for epoch in range(1, args.n_epochs + 1):
+        print(time.ctime(), f'Fold {fold}, Epoch {epoch}')
+#         scheduler_warmup.step(epoch - 1)
 
-    def train(self):
-        for epoch in range(1, self.N_EPOCH+1):
-            for (_,train) , (_,val) in zip(enumerate(self.train_data, 0),enumerate(self.val_data, 0)):
-                self.model.train()
-                X_img_train_ts,X_tab_train_ts, y_train_ts = train
-                X_img_val_ts, X_tab_val_ts,y_val_ts = val
+        train_loss = train_epoch(model, train_loader, optimizer)
+        val_loss, acc, auc, auc_20 = val_epoch(model, valid_loader, mel_idx, is_ext=df_valid['is_ext'].values)
+        if args.wandb:
+            wandb.log(
+                {"Epoch" : epoch,
+                 "Fold" : fold,
+                 "Train Loss": train_loss,
+                 "Validation Loss": val_loss,
+                 "Accuracy": acc
+                })            
 
-                X_img_train_ts,X_tab_train_ts, y_train_ts  = X_img_train_ts.to(self.device),X_tab_train_ts.to(self.device), y_train_ts.to(self.device)
-                X_img_val_ts, X_tab_val_ts,y_val_ts = X_img_val_ts.to(self.device), X_tab_val_ts.to(self.device),y_val_ts.to(self.device) 
-                
+        content = time.ctime() + ' ' + f'Fold {fold}, Epoch {epoch}, lr: {optimizer.param_groups[0]["lr"]:.7f}, train loss: {train_loss:.5f}, valid loss: {(val_loss):.5f}, acc: {(acc):.4f}, auc: {(auc):.6f}, auc_20: {(auc_20):.6f}.'
+        print(content)
+        with open(os.path.join(args.log_dir, f'log_{args.kernel_type}.txt'), 'a') as appender:
+            appender.write(content + '\n')
 
-                out = self.model(X_img_train_ts,X_tab_train_ts)      
-                loss = self.criterion(out, y_train_ts)
-                
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-                
-                if self.mode == 'classification':
-                    acc = (th.argmax(out, dim=1) == y_train_ts).float().mean().item()
-                
-                self.model.eval()
-                with th.no_grad():
-                    out_val = self.model(X_img_val_ts,X_tab_val_ts)
-                    loss_val = self.criterion(out_val, y_val_ts)
-                    if self.mode == 'classification':
-                        acc_val = (th.argmax(out_val, dim=1) == y_val_ts).float().mean().item()
-               
-            self.writer1.add_scalar('Loss', loss.item(), epoch)
-            self.writer2.add_scalar('Loss', loss_val.item(), epoch)  
-                       
-            if self.mode == 'classification':
-                self.writer1.add_scalar('Accuracy', acc*100 , epoch)
-                self.writer2.add_scalar('Accuracy', acc_val*100, epoch)                           
-
-            if epoch % 20 == 0:
-                    if self.mode == 'classification':
-                        print('Epoch : {:3d} / {}, Loss : {:.4f}, Accuracy : {:.2f} %, Val Loss : {:.4f}, Val Accuracy : {:.2f} %'.format(
-                            epoch, self.N_EPOCH, loss.item(), acc*100, loss_val.item(), acc_val*100))
-                    else:
-                        print('Epoch : {:3d} / {}, Loss : {:.4f},  Val Loss : {:.4f}'.format(
-                            epoch, self.N_EPOCH, loss.item(), loss_val.item()))
-                
-        self.writer1.close()        
-        self.writer2.close()        
-
-    # def test(self):
-    #     acc_avg = 0
-    #     loss_avg = 0
-    #     y_list = [] # i,y_test,y_pred
-        
-    #     for (i,test) in enumerate(self.test_data, 0):
+        scheduler_warmup.step()    
+        if epoch==2: scheduler_warmup.step() # bug workaround   
             
-    #         X_test_ts, y_test_ts = test           
-    #         X_test_ts, y_test_ts = X_test_ts.to(self.device), y_test_ts.to(self.device)
+        if auc > auc_max:
+            print('auc_max ({:.6f} --> {:.6f}). Saving model ...'.format(auc_max, auc))
+            torch.save(model.state_dict(), model_file)
+            auc_max = auc
+        if auc_20 > auc_20_max:
+            print('auc_20_max ({:.6f} --> {:.6f}). Saving model ...'.format(auc_20_max, auc_20))
+            torch.save(model.state_dict(), model_file2)
+            auc_20_max = auc_20
 
-            
-    #         self.model.eval()
-    #         with th.no_grad():
-    #             out_test = self.model(X_test_ts.to(th.float32))
-    #             loss_test = self.criterion(out_test, y_test_ts)
-    #             if self.mode == 'classification':
-    #                 acc_test = (th.argmax(out_test, dim=1) == y_test_ts).float().mean().item()
-            
-    #         if self.mode == 'classification':
-    #             acc_avg += acc_test            
-    #         else:
-    #             y_list.append([i,y_test_ts.cpu().numpy(),out_test.cpu().numpy()])                
-                
-    #         loss_avg += loss_test
-    #     if self.mode == 'classification':
-    #         print('Val avg Loss : {:.4f}, Val avg Accuracy : {:.2f} %'.format(
-    #             loss_avg/i,acc_avg/i*100))  
-    #     else:
-    #         print('Val avg Loss : {:.4f}'.format(
-    #             loss_avg/i))  
-    #         return     
-
-
-    
+    torch.save(model.state_dict(), model_file3)
